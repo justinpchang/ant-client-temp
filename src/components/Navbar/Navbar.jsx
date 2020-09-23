51ff89f2612e2e5d55f7d8f0d182fcbcf4436c35
Justin Chang <justin.p.chang@gmail.com>
2020-09-22 21:20:20 -0400

    Add styled-components and navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+  NavbarBackground,
+} from './Navbar.styles';
+
+function Navbar(props) {
+  return (
+    <NavbarBackground>
+      <p>This is the navbar</p>
+    </NavbarBackground>
+  );
+}
+
+export default Navbar;
